4457014cb56fb8c95762a85b28b4255c6b3ad163
Christian Llupo <jebediah47@protonmail.com>
2025-10-04 11:48:04 +0300

    Set default value for `Fact` property in `CatFactModel`.

--- GalacticaBot/Models/CatFactModel.cs
+++ GalacticaBot/Models/CatFactModel.cs
@@ -5,7 +5,7 @@ namespace GalacticaBot.Models;
 public sealed class CatFactModel
 {
     [JsonPropertyName("fact")]
-    public string Fact { get; set; }
+    public string Fact { get; set; } = string.Empty;
 
     [JsonPropertyName("length")]
     public int Length { get; set; }
