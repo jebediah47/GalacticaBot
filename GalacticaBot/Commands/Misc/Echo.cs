d3afa9c727a3c3ad7cc2cb33454dc4ffa2e4628c
Christian Llupo <jebediah47@protonmail.com>
2025-10-04 09:59:33 +0300

    Add echo command

---
+++ GalacticaBot/Commands/Misc/Echo.cs
@@ -0,0 +1,16 @@
+﻿using NetCord.Rest;
+using NetCord.Services.ApplicationCommands;
+
+namespace GalacticaBot.Commands.Misc;
+
+public sealed class Echo : ApplicationCommandModule<ApplicationCommandContext>
+{
+    [SlashCommand("echo", "Echoes your message")]
+    public Task Run(
+        [SlashCommandParameter(Name = "message", Description = "The message to echo")]
+            string message
+    )
+    {
+        return RespondAsync(InteractionCallback.Message(message));
+    }
+}
