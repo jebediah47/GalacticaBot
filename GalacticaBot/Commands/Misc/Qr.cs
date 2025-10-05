a7e03a8926ffccf6a308fb45b06ac7d258a79698
Christian Llupo <jebediah47@protonmail.com>
2025-10-05 13:14:22 +0300

    Add Qr command

---
+++ GalacticaBot/Commands/Misc/Qr.cs
@@ -0,0 +1,35 @@
+﻿using GalacticaBot.Utils;
+using NetCord.Rest;
+using NetCord.Services.ApplicationCommands;
+
+namespace GalacticaBot.Commands.Misc;
+
+public sealed class Qr : ApplicationCommandModule<ApplicationCommandContext>
+{
+    [SlashCommand("qr", "Generates a QR code based on the provided text")]
+    public async Task Run(
+        [SlashCommandParameter(Name = "text", Description = "The text to encode into a QR code")]
+            string text
+    )
+    {
+        var interaction = Context.Interaction;
+        await interaction.SendResponseAsync(
+            InteractionCallback.Message("Please wait while your text is converted to a QR-code")
+        );
+
+        var encodedText = Uri.EscapeDataString(text);
+
+        var embed = new EmbedProperties()
+            .WithColor(RandomColor.Get())
+            .WithImage(
+                $"https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=${encodedText}"
+            )
+            .WithTimestamp(DateTimeOffset.UtcNow);
+
+        await interaction.ModifyResponseAsync(msg =>
+        {
+            msg.Content = string.Empty;
+            msg.Embeds = [embed];
+        });
+    }
+}
