53f9fb7210ae3ec43898293e6afe50d3f2d5253f
Christian Llupo <jebediah47@protonmail.com>
2025-10-06 18:55:01 +0300

    Add ascii command

---
+++ GalacticaBot/Commands/Fun/Ascii.cs
@@ -0,0 +1,19 @@
+﻿using Figgle.Fonts;
+using NetCord.Rest;
+using NetCord.Services.ApplicationCommands;
+
+namespace GalacticaBot.Commands.Fun;
+
+public sealed class Ascii : ApplicationCommandModule<ApplicationCommandContext>
+{
+    [SlashCommand("ascii", "Converts text to ASCII art")]
+    public Task Run(
+        [SlashCommandParameter(Name = "text", Description = "The text to convert", MaxLength = 20)]
+            string text
+    )
+    {
+        var asciiArt = FiggleFonts.Standard.Render(text);
+
+        return RespondAsync(InteractionCallback.Message($"```{asciiArt}```"));
+    }
+}
